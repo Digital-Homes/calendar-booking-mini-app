57b7a5e6e664c577f6a82d87f561b1f94a572266
admrbsn <admrbsn@gmail.com>
2024-09-17 10:20:40 -0600

    Merging into a single app.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { plugin, defaultConfig } from '@formkit/vue';
 import App from './App.vue';
 import formKitConfig from '../formkit.config';
 import { createRouter, createWebHistory } from 'vue-router';
+import Authenticate from './components/Authenticate.vue';
 import BookingForm from './components/BookingForm.vue';
 import Products from './components/Products.vue';
 
 const routes = [
+  { path: '/authenticate', component: Authenticate },
   { path: '/', component: BookingForm },
   { path: '/products', component: Products },
 ];
